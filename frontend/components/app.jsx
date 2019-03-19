f59c610a8190370fde6225fddfb3f654baa76100
MikeG212 <michaelcgee93@gmail.com>
2019-03-18 17:15:16 -0700

    Create log out button in navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,8 +14,6 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = ({ children }) => (
   <div>
-    <Link to="/" className="nav-brand-logo">
-    </Link>
     <NavBarContainer />
     {children}
     <Switch>
--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -9,14 +9,36 @@ const NavBar = ({ currentUser, logout }) => {
             <Link to="/signup">Sign up!</Link>
         </nav>
     );
-    const personalGreeting = () => (
-        <hgroup className="header-group">
-            <h2 className="header-name">Oh Hi {currentUser.username}!</h2>
-            <button className="header-button" onClick={logout}>Log Out</button>
-        </hgroup>
+
+    const navBar = () => (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <a className="navbar-brand core-sprite hide-text" href="#">Spidergram</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+                <button className="btn btn-outline-success logout my-2 my-sm-0" type="submit" onClick={logout}>Log Out</button>
+            </div>
+        </nav>
     );
 
-    return currentUser ? personalGreeting() : sessionLinks();
+    return currentUser ? navBar() : sessionLinks();
 };
 
 export default NavBar;
