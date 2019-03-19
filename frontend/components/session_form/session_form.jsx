a895a87f3ae1308a1b5ee929bb1047d2fa7caf71
MikeG212 <michaelcgee93@gmail.com>
2019-03-19 15:17:39 -0700

    Style log in form

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -66,51 +66,63 @@ class SessionForm extends React.Component {
     }
 
     render() {
-
         return (
-            <div className="page">
-                <div className="session-form-container group">
-                    <img className="session-logo" src={window.images.dummyPhone} alt="dummyPhone"></img>
-                    <div className="session-right-side">
-                        <form onSubmit={this.handleSubmit} className="session-form-box">
-                            <div className="sessionForm">
-                                <h1>Welcome to Spidergram!</h1>
-                                <div className="session-form-text">Please {this.props.formType} or {this.navLink()}</div>
-                                {this.renderErrors()}
-                                    <label htmlFor="email">Email:
-                                        <input type="text"
-                                            value={this.state.email}
-                                            onChange={this.update('email')}
-                                            className="form-input"
-                                        />
-                                    </label>
-                                    <br />
-                                    <br />
-                                    <label htmlFor="username">Username:
-                                        <input type="text"
-                                            value={this.state.username}
-                                            onChange={this.update('username')}
-                                            className="form-input"
-                                        />
-                                    </label>
-                                    <br />
-                                    <label htmlFor="password">Password:
-                                        <input type="password"
-                                            value={this.state.password}
-                                            onChange={this.update('password')}
-                                            className="form-input"
-                                        />
-                                    </label>
-                                    <br />
-                                    <input className="session-button submit-button" type="submit" value={this.props.formType} />
-                                    <br />
-                                    {this.demoLoginButton()}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 landing-left">
+                            <div className="dummy-phone">
+                                <div className="screen-shot">
+                                    <div className="carousel carousel-fade" data-ride="carousel" >
+                                        <div className="carousel-inner">
+                                            <img className="carouse-item active screen-shot" src={window.images.screenshot1} alt="dummyPhone1"></img>
+                                            <img className="carousel-item screen-shot" src={window.images.screenshot2} alt="dummyPhone2"></img>
+                                            <img className="carousel-item screen-shot" src={window.images.screenshot3} alt="dummyPhone3"></img>
+                                            <img className="carousel-item screen-shot" src={window.images.screenshot4} alt="dummyPhone4"></img>
+                                            <img className="carousel-item screen-shot" src={window.images.screenshot5} alt="dummyPhone5"></img>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </form>
+                        </div>
+                        <div className="col-lg-6 landing-right text-center d-flex align-items-center">
+                            <div className="session-form-container">
+                                <form onSubmit={this.handleSubmit} className="session-form-box">
+                                <h3>Spidergram</h3>
+                                    <div className="session-form text">Please {this.props.formType} or {this.navLink()}</div>
+                                    {this.renderErrors()}
+                                        <label htmlFor="email">Email:
+                                            <input type="text"
+                                                value={this.state.email}
+                                                onChange={this.update('email')}
+                                                className="form-input"
+                                            />
+                                        </label>
+                                        <br />
+                                        <label htmlFor="username">Username:
+                                            <input type="text"
+                                                value={this.state.username}
+                                                onChange={this.update('username')}
+                                                className="form-input"
+                                            />
+                                        </label>
+                                        <br />
+                                        <label htmlFor="password">Password:
+                                            <input type="password"
+                                                value={this.state.password}
+                                                onChange={this.update('password')}
+                                                className="form-input"
+                                            />
+                                        </label>
+                                        <br />
+                                        <input className="session-button submit-button" type="submit" value={this.props.formType} />
+                                        <br />
+                                    {this.demoLoginButton()}
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+                );
     }
 }
 
