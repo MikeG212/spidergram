43d42e810714ea593685f6ebea7a7a0f4dd57906
MikeG212 <michaelcgee93@gmail.com>
2019-04-17 11:58:07 -0700

    Add script to package json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
 User.destroy_all
 Post.destroy_all
 
